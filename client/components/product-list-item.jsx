d774e9b434712e880bb01abf8f4bde3c1a3ad152
Jeffery Jones <yreffej94@gmail.com>
2020-03-10 16:15:57 -0700

    Added classes to avoid modifying bootstrap classes

--- client/components/product-list-item.jsx
+++ client/components/product-list-item.jsx
@@ -7,13 +7,13 @@ export default class ProductListItem extends React.Component {
     ${price.slice(0, price.length - 2)}.${price.slice(price.length - 2)}`;
 
     return (
-      <div id={this.props.productId} className='card col-12 flex-column'>
+      <div id={this.props.productId} className='card card-unpad col-12 flex-column'>
         <div className='card-img-container'>
-          <img src={this.props.image} className='h-100 flex-grow-1 card-img-top' alt={this.props.name}/>
+          <img src={this.props.image} className='h-100 flex-grow-1 card-img-top item-img' alt={this.props.name}/>
         </div>
-          <div className='card-body'>
-            <h5 className='card-title'>{this.props.name}</h5>
-            <p className='card-text text-muted'>{formattedPrice}</p>
+          <div className='card-body card-body-unpad'>
+            <h5 className='card-title title-text'>{this.props.name}</h5>
+            <p className='card-text card-text-small text-muted'>{formattedPrice}</p>
             <p className='card-text'>{this.props.shortDescription}</p>
           </div>
       </div>
