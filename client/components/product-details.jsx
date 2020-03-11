0fa1669ef2c4b88d749ded4b11bc4b17404e11a8
Jeffery Jones <yreffej94@gmail.com>
2020-03-10 17:42:03 -0700

    Added ProductDetails component with conditional rendering that waits for fetch request

---
+++ client/components/product-details.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+
+export default className ProductDetails extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      product: null
+    }
+  }
+
+  componentDidMount() {
+    fetch('/api/products/' + this.props.productId)
+      .then(response => response.json())
+      .then(data => {
+        this.setState({
+          product: data
+        })
+      })
+  }
+
+  render(){
+    if (this.state.product === null) return null;
+
+    return (
+      <div className='modal' tabIndex='-1' role='dialog'>
+        <div className='modal-dialog' role='document'>
+          <div className='modal-content'>
+            <div className='modal-header'>
+              <h5 className='modal-title'>Modal title</h5>
+              <button type='button' className='close' dataDismiss='modal' ariaLabel='Close'>
+                <span ariaHidden='true'>&times;</span>
+              </button>
+            </div>
+            <div className='modal-body'>
+              <p>Modal body text goes here.</p>
+            </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
