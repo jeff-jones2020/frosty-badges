1b351a4dcf9c68ff2b6d330e73f37aec2d7bca85
Jeffery Jones <56287988+jeff-jones2020@users.noreply.github.com>
2020-04-15 11:23:17 -0700

    Removed hover effect from cart summary items, added setView callback to cart summary items

--- client/components/cart-summary.jsx
+++ client/components/cart-summary.jsx
@@ -8,7 +8,13 @@ export default class CartSummary extends React.Component {
   }
 
   setView(e) {
-    if (e.target.id === 'checkout-btn') { this.props.setViewCallback('checkout', {}); } else { this.props.setViewCallback('catalog', {}); }
+    if (e.target.id === 'checkout-btn') {
+      this.props.setViewCallback('checkout', {});
+    } else if (e.target.id === 'back-to-catalog') {
+      this.props.setViewCallback('catalog', {});
+    } else {
+      this.props.setViewCallback('details', { productId: e.currentTarget.id });
+    }
   }
 
   render() {
@@ -23,7 +29,8 @@ export default class CartSummary extends React.Component {
           price={item.price}
           image={item.image}
           shortDescription={item.shortDescription}
-          quantity={item.quantity} />
+          quantity={item.quantity}
+          setViewCallback={this.setView}/>
       );
     });
 
@@ -67,8 +74,8 @@ function CartSummaryItem(props) {
     ${price.slice(0, price.length - 2)}.${price.slice(price.length - 2)}`;
 
   return (
-    <div id={props.productId} onClick={props.setViewCallback} className='card card-unpad col-12 d-flex flex-row mb-3'>
-      <div className='card-img-container col-4 d-flex justify-content-center'>
+    <div className='card col-12 d-flex flex-row mb-3'>
+      <div onClick={props.setViewCallback} id={props.productId} className='summary-img card-img-container col-4 d-flex justify-content-center'>
         <img src={props.image} className='h-100 flex-grow-1 img-fluid item-img' alt={props.name} />
       </div>
       <div className='card-body card-body-unpad'>
