ca0e676822e30560f822fb8f25bdc12f19b267b3
Jeffery Jones <56287988+jeff-jones2020@users.noreply.github.com>
2020-04-17 23:19:44 -0700

    Added front end form validation and feedback for checkout form

--- client/components/checkout-form.jsx
+++ client/components/checkout-form.jsx
@@ -7,81 +7,80 @@ export default class CheckoutForm extends React.Component {
     this.state = {
       name: '',
       nameShort: true,
+      nameMsgVisible: false,
       creditCard: '',
       creditCardShort: true,
+      cardMsgVisible: false,
       shippingAddress: '',
       shippingAddressShort: true,
+      addressMsgVisible: false,
       orderDisabled: true
     };
 
-    this.placeOrder = this.placeOrder.bind(this);
+    this.nameMissingMsg = 'Full Name is required.';
+    this.nameShortMsg = 'Full Name must be at least 5 characters long.';
+    this.cardMissingMsg = 'Credit Card number is required.';
+    this.cardShortMsg = 'Please enter a valid credit card number.';
+    this.addressMissingMsg = 'Shipping address is required.';
+    this.addressShortMsg = 'Address must be at least 21 characters long.';
+
     this.updateStateText = this.updateStateText.bind(this);
+    this.revealMessage = this.revealMessage.bind(this);
+    this.placeOrder = this.placeOrder.bind(this);
     this.setView = this.setView.bind(this);
-    this.validateAndCorrect = this.validateAndCorrect.bind(this);
   }
 
   updateStateText(e) {
-    if (!this.validateAndCorrect(e.target)) return;
-    const { name, creditCard, shippingAddress, nameShort, creditCardShort, shippingAddressShort } = this.state;
-    const newInputs = { name, creditCard, shippingAddress };
-    newInputs[e.target.id] = e.target.value;
-
-    const disable = nameShort || creditCardShort || shippingAddressShort;
-    this.setState({
-      name: newInputs.name,
-      creditCard: newInputs.creditCard,
-      shippingAddress: newInputs.shippingAddress,
-      orderDisabled: disable
-    });
-
-  }
-
-  validateAndCorrect(inputEl) {
-    let isValid = true;
+    const { nameShort, creditCardShort, shippingAddressShort } = this.state;
+    const inputEl = e.target;
+    let orderDisabled;
     if (inputEl.id === 'name') {
-      let name = inputEl.value.trim();
+      let name = inputEl.value;
+      const trimmedName = inputEl.value.trim();
       let nameShort = false;
-      if (name.length < 5) {
-        nameShort = !nameShort;
-        isValid = false;
-      } else if (name.length >= 65) {
+      if (trimmedName.length < 5) {
+        nameShort = true;
+      } else if (trimmedName.length >= 65) {
         name = name.slice(0, name.length - 1);
-        isValid = false;
       }
-      this.setState({ name, nameShort });
-      return isValid;
+      orderDisabled = nameShort || creditCardShort || shippingAddressShort;
+      this.setState({ name, nameShort, orderDisabled, nameMsgVisible: false });
     } else if (inputEl.id === 'creditCard') {
       let creditCard = inputEl.value.trim().replace(/[-]/g, '');
       let creditCardShort = false;
       if (!creditCard.length) {
-        isValid = false;
+        creditCardShort = true;
       } else if (creditCard.length > 16 || !creditCard[creditCard.length - 1].match(/[0-9]/)) {
         creditCard = creditCard.slice(0, creditCard.length - 1);
-        isValid = false;
       } else if (creditCard.length < 16) {
-        creditCardShort = !creditCardShort;
-        isValid = false;
+        creditCardShort = true;
       }
       const newLength = creditCard.length + Math.floor(creditCard.length / 4 - 1);
       for (let i = 4; i < newLength; i += 4) {
         creditCard = creditCard.slice(0, i) + '-' + creditCard.slice(i, creditCard.length);
         i++;
       }
-      this.setState({ creditCard, creditCardShort });
-      return isValid;
+      orderDisabled = nameShort || creditCardShort || shippingAddressShort;
+      this.setState({ creditCard, creditCardShort, orderDisabled, cardMsgVisible: false });
     } else if (inputEl.id === 'shippingAddress') {
       let shippingAddress = inputEl.value;
+      const trimmedAddress = inputEl.value.trim();
       let shippingAddressShort = false;
-      if (shippingAddress.length < 21) {
-        shippingAddressShort = !shippingAddressShort;
-        isValid = false;
-      } else if (shippingAddress.length > 156) {
+      if (trimmedAddress.length < 21) {
+        shippingAddressShort = true;
+      } else if (trimmedAddress.length > 156) {
         shippingAddress = shippingAddress.slice(0, shippingAddress.length - 1);
-        isValid = false;
       }
-      this.setState({ shippingAddress, shippingAddressShort });
-      return isValid;
+      orderDisabled = nameShort || creditCardShort || shippingAddressShort;
+      this.setState({ shippingAddress, shippingAddressShort, orderDisabled, addressMsgVisible: false });
     }
+  }
+
+  revealMessage(e) {
+    const visibilityPropName = e.target.getAttribute('data-vis');
+    this.setState({
+      [`${visibilityPropName}`]: true
+    });
   }
 
   placeOrder(e) {
@@ -103,6 +102,19 @@ export default class CheckoutForm extends React.Component {
     totalPrice = (totalPrice / 100).toFixed(2); // convert to dollar amount
     const priceFormatted = '$' + totalPrice;
 
+    const errMessages = [null, null, null];
+    if (this.state.orderDisabled) {
+      if (this.state.nameShort) {
+        errMessages[0] = this.state.name.length ? this.nameShortMsg : this.nameMissingMsg;
+      }
+      if (this.state.creditCardShort) {
+        errMessages[1] = this.state.creditCard.length ? this.cardShortMsg : this.cardMissingMsg;
+      }
+      if (this.state.shippingAddressShort) {
+        errMessages[2] = this.state.shippingAddress.length ? this.addressShortMsg : this.addressMissingMsg;
+      }
+    }
+
     return (
       <section id='checkout-form' className='d-flex flex-column col-md-8 col-lg-6 col-xl-4 col-12 mx-auto'>
         <h1 className='my-4'>Checkout</h1>
@@ -113,13 +125,20 @@ export default class CheckoutForm extends React.Component {
         <form className='mb-4'>
           <div className='form-group'>
             <label htmlFor='name'>Full Name</label>
-            <input type='text'
+            <input
+              type='text'
               name='name'
               value={this.state.name}
               onChange={this.updateStateText}
+              onBlur={this.revealMessage}
               className='form-control'
               placeholder='John M Doe'
+              data-vis='nameMsgVisible'
               id='name' />
+            <div
+              className={`validation-err text-danger ${this.state.nameMsgVisible ? '' : 'invisible'}`}>
+              {errMessages[0]}
+            </div>
           </div>
           <div className='form-group'>
             <label htmlFor='credit-card'>Credit Card</label>
@@ -128,9 +147,14 @@ export default class CheckoutForm extends React.Component {
               name='credit-card'
               value={this.state.creditCard}
               onChange={this.updateStateText}
+              onBlur={this.revealMessage}
               className='form-control'
               placeholder='0000-0000-0000-0000'
+              data-vis='cardMsgVisible'
               id='creditCard' />
+            <div className={`validation-err text-danger ${this.state.cardMsgVisible ? '' : 'invisible'}`}>
+              {errMessages[1]}
+            </div>
           </div>
           <div className='form-group'>
             <label htmlFor='shipping-address'>Shipping Address</label>
@@ -139,9 +163,14 @@ export default class CheckoutForm extends React.Component {
               name='shipping-address'
               value={this.state.shippingAddress}
               onChange={this.updateStateText}
+              onBlur={this.revealMessage}
               className='form-control'
               placeholder='1234 Example Street, Irvine CA 92618'
+              data-vis='addressMsgVisible'
               id='shippingAddress' />
+            <div className={`validation-err text-danger ${this.state.addressMsgVisible ? '' : 'invisible'}`}>
+              {errMessages[2]}
+            </div>
           </div>
         </form>
         <div id='checkout-footer' className='d-flex flex-row justify-content-between mb-4'>
